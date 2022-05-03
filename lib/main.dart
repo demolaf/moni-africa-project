b4b61d83b5482482a6347d41d629411f64454bd0
Ademola <demolafadumo@gmail.com>
2022-05-03 13:59:55 +0100

    navigation and riverpod setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:moni_africa_project/src/ui/views/startup/startup_view.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moni_africa_project/src/core/navigation.dart';
+import 'package:moni_africa_project/src/ui/core/constants/strings.dart';
+import 'package:moni_africa_project/src/ui/core/routes.dart';
 
 void main() {
   runApp(
-    const MoniAfrica(),
+    const ProviderScope(
+      child: MoniAfrica(),
+    ),
   );
 }
 
-class MoniAfrica extends StatelessWidget {
+class MoniAfrica extends ConsumerWidget {
   const MoniAfrica({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Moni Africa Project',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const StartupView(),
+      title: AppStrings.kTitle,
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: Routes.generateRoute,
+      initialRoute: Routes.startupView,
+      navigatorKey: ref.read(navigationProvider).navigatorKey,
     );
   }
 }
