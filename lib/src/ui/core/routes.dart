a9aaf2e88af0702487b96235b2f6f34b668f39fc
Ademola <demolafadumo@gmail.com>
2022-05-04 18:17:30 +0100

    added routes for get started and my cluster

--- lib/src/ui/core/routes.dart
+++ lib/src/ui/core/routes.dart
@@ -2,12 +2,16 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:moni_africa_project/src/ui/views/home/my_cluster/my_cluster_view.dart';
+import 'package:moni_africa_project/src/ui/views/main/get_started/get_started_view.dart';
 
 import '../views/startup/startup_view.dart';
 
 /// Routes
 class Routes {
   static const startupView = '/startup_view';
+  static const getStartedView = '/get_started_view';
+  static const myClusterView = '/my_cluster_view';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     if (!Platform.isIOS) {
@@ -15,6 +19,14 @@ class Routes {
         case startupView:
           return MaterialPageRoute(
             builder: (_) => const StartupView(),
+          );
+        case getStartedView:
+          return MaterialPageRoute(
+            builder: (_) => const GetStartedView(),
+          );
+        case myClusterView:
+          return MaterialPageRoute(
+            builder: (_) => const MyClusterView(),
           );
         default:
           return MaterialPageRoute(
@@ -30,6 +42,14 @@ class Routes {
         case startupView:
           return CupertinoPageRoute(
             builder: (_) => const StartupView(),
+          );
+        case getStartedView:
+          return CupertinoPageRoute(
+            builder: (_) => const GetStartedView(),
+          );
+        case myClusterView:
+          return CupertinoPageRoute(
+            builder: (_) => const MyClusterView(),
           );
         default:
           return CupertinoPageRoute(
