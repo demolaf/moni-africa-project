4ab023a27e21866fa8531e5f2395ad28f8f82eb0
Ademola <demolafadumo@gmail.com>
2022-05-04 18:19:23 +0100

    app colors added

---
+++ lib/src/ui/core/constants/colors.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  // Standard colors
+  static const kWhite = Color(0xFFFFFFFF);
+  static const kBlack = Color(0xFF000000);
+  static const kPrimary = Color(0xFFE66652);
+  static const kSecondary = Color(0xFF7DDE86);
+
+  // More colors
+  static const kRed100 = Color(0xFFE41002);
+
+  static const kGreen200 = Color(0xFF198155);
+  static const kGreen100 = Color(0xFF23C16B);
+
+  static const kYellow300 = Color(0xFFA05E03);
+  static const kYellow200 = Color(0xFFEAB948);
+  static const kYellow100 = Color(0xFFF0CC79);
+  static const kYellow50 = Color(0xFFFDF8ED);
+
+  static const kGrey500 = Color(0xFF72777A);
+  static const kGrey400 = Color(0xFF979C9E);
+  static const kGrey300 = Color(0xFFC4C4C4);
+  static const kGrey200 = Color(0xFFCDCFD0);
+  static const kGrey100 = Color(0xFFE3E5E5);
+
+  static const kBlue300 = Color(0xFF13163E);
+  static const kBlue200 = Color(0xFF4F6482);
+  static const kBlue100 = Color(0xFFEBF1F9);
+}
