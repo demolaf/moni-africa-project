2f4f94728c9e09369b1712656d00e32c471c7c40
Ademola <demolafadumo@gmail.com>
2022-05-04 18:16:27 +0100

    startup view impl

--- lib/src/ui/views/startup/startup_view.dart
+++ lib/src/ui/views/startup/startup_view.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moni_africa_project/src/ui/core/constants/colors.dart';
+import 'package:moni_africa_project/src/ui/core/constants/strings.dart';
+import 'package:moni_africa_project/src/ui/core/constants/text_styles.dart';
+import 'package:moni_africa_project/src/ui/shared/stateless/circular_loading_indicator.dart';
+import 'package:moni_africa_project/src/ui/shared/stateless/gap.dart';
+import 'package:moni_africa_project/src/ui/views/startup/startup_viewmodel.dart';
 
-class StartupView extends StatelessWidget {
+class StartupView extends ConsumerStatefulWidget {
   const StartupView({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<StartupView> createState() => _StartupViewState();
+}
+
+class _StartupViewState extends ConsumerState<StartupView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(startupViewModel).initialize();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColors.kWhite,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              AppStrings.kLoading,
+              style: AppTextStyles.kBodyMedium.copyWith(
+                color: AppColors.kPrimary,
+              ),
+            ),
+            Gap.sm,
+            const ReusableProgressIndicator(
+              color: AppColors.kPrimary,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/src/ui/views/startup/startup_viewmodel.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moni_africa_project/src/core/navigation.dart';
+import 'package:moni_africa_project/src/ui/core/routes.dart';
+
+final startupViewModel = Provider<StartupViewModel>(
+  (ref) => StartupViewModel(ref.read),
+);
+
+class StartupViewModel extends ChangeNotifier {
+  StartupViewModel(this._reader) : super();
+
+  final Reader _reader;
+
+  /// Any startup initialization can be done here
+  void initialize() async {
+    await Future.delayed(const Duration(seconds: 2)).then(
+      (value) => _reader(navigationProvider)
+          .pushNamedAndRemoveUntil(Routes.getStartedView, (_) => false),
+    );
+  }
+}
