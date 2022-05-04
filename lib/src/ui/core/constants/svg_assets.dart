27d257e88ecd432ded5dcc15e47d7dbbd9dfdc3d
Ademola <demolafadumo@gmail.com>
2022-05-04 18:21:09 +0100

    icons added

---
+++ lib/src/ui/core/constants/svg_assets.dart
@@ -0,0 +1,11 @@
+class SvgAssets {
+  SvgAssets._();
+
+  static const badgeDollarIcon = 'assets/icons/badge_dollar.svg';
+  static const linkIcon = 'assets/icons/link_icon.svg';
+  static const multiListIcon = 'assets/icons/multi_list_icon.svg';
+  static const settingsIcon = 'assets/icons/settings_icon.svg';
+  static const editIcon = 'assets/icons/edit_icon.svg';
+  static const moneyBillWaveIcon = 'assets/icons/money_bill_wave_icon.svg';
+  static const viewIcon = 'assets/icons/view_icon.svg';
+}
