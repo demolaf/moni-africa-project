bd445f263cab8936b2acb291f4d6d63feb6f79ef
Ademola <demolafadumo@gmail.com>
2022-05-04 18:22:08 +0100

    image assets added

---
+++ lib/src/ui/core/constants/image_assets.dart
@@ -0,0 +1,11 @@
+class ImageAssets {
+  ImageAssets._();
+
+  static const clusterHeaderBackground =
+      'assets/images/my_cluster_header_background.png';
+  static const placeHolderClusterHeader =
+      'assets/images/placeholder_cluster_header.png';
+  static const placeHolder2ClusterHeader =
+      'assets/images/placeholder_2_cluster_header.png';
+  static const moniGetStarted = 'assets/images/moni_get_started.png';
+}
