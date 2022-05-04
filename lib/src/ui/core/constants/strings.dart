6a892ec4094a0216d71408bffb204baf3d4d1d25
Ademola <demolafadumo@gmail.com>
2022-05-04 18:21:32 +0100

    app strings extracted

---
+++ lib/src/ui/core/constants/strings.dart
@@ -0,0 +1,48 @@
+class AppStrings {
+  AppStrings._();
+
+  static const kTitle = 'Moni Africa App';
+  static const kMyCluster = 'My cluster';
+  static const kPendingJoinRequest = 'Pending join request';
+  static const kRepaymentDay = 'Repayment day';
+  static const kTotalLoanCollectedByCluster = 'Total loan collected by cluster';
+  static const kChangeTextLink = 'Change';
+  static const kMembersTab = 'Members';
+  static const kClusterDetails = 'Cluster Details';
+  static const kMoniDreamBigCommunity = 'Moni dreambig community';
+  static const kRepaymentRate = 'Repayment rate';
+  static const kClusterPurseBalance = 'Cluster purse balance';
+  static const kInterestToday = 'interest today';
+  static const kViewPurse = 'View Purse';
+  static const kTotalInterestEarned = 'Total interest earned';
+  static const kTotalOwedByMembers = 'Total owed by members';
+  static const kOverdueLoansHeader = 'Overdue loans';
+  static const kDueTodayHeader = 'Due today';
+  static const kActiveLoansHeader = 'Active loans';
+  static const kInactiveLoansHeader = 'Inactive loans';
+  static const kLateLoan = 'Late Loan';
+  static const kActiveLoan = 'Active Loan';
+  static const kDueToday = 'Due Today';
+  static const kNoActiveLoan = 'No active loan';
+  static const kLoanSettings = 'Loan settings';
+  static const kGroupSettings = 'Group settings';
+  static const kGroupRules = 'Group rules';
+  static const kGroupWhatsapp = 'Group Whatsapp';
+  static const kEditSettingsTextLink = 'Edit Settings';
+  static const kGroupInviteLinkCode = 'Group invite link/code';
+  static const kUseTheLinkToInvite =
+      'Use the link or code below to invite new member';
+  static const kMemberInviteCode = 'Member invite code';
+  static const kGetNewCodeTextLink = 'Get new code';
+  static const kClusterPurseSetting = 'Cluster purse setting';
+  static const kFrequencyOfContribution = 'Frequency of contribution';
+  static String kYourContributionAmount(int percent) =>
+      'Your contribution is $percent% of your eligible amount';
+  static const kBenefitsEarned = 'Benefits earned';
+  static const kTotalCHBenefitsEarned = 'Total CH Benefits earned';
+  static const kAvailableBenefits = 'Available Benefits';
+  static const kViewEarningHistory = 'View earning history';
+  static const kGetStarted = 'Get Started';
+  static const kLoading = 'Loading...';
+  static const kNoPendingClusterJoinRequest = 'No pending cluster join request';
+}
